2f630493f194a171021797d1da9178808391a42f
uribo <ur1b0.dev@gmail.com>
2022-12-20 05:22:47 +0900

    複数のチャネルにデータを上げるようにした

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import requests
 import json
+import ambient
+import time
 
 dataKeys = ["d1","d2","d3","d4","d5","d6","d7"]
 
 with open("./config.json") as f:
     conf = json.loads(f.read())
 
-print(f"conf:{conf}")
 channelId = conf["source"]["ambient_channel"]
 readKey = conf["source"]["ambient_key_read"]
 
@@ -19,5 +20,11 @@ print(res.status_code)
 d = res.json()[0]
 
 data = {i:d[i] for i in d.keys() if i in dataKeys}
+
+for i in conf["clients"]:
+    am = ambient.Ambient(i["ambient_channel"],i["ambient_key_write"])
+    res = am.send(data,timeout=3)
+    time.sleep(3)
+    print(f"ch:{i['ambient_channel']},key:{i['ambient_key_write']}")
 
 print(data)
