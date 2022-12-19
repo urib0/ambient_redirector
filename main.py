ed66366c378150e3a83a1967420f42836edfe0a3
uribo <ur1b0.dev@gmail.com>
2022-12-20 05:29:05 +0900

    周期動作するようにした

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import ambient
 import time
 
-dataKeys = ["d1","d2","d3","d4","d5","d6","d7"]
+dataKeys = ["d1","d2","d3","d4","d5","d6","d7","d8"]
 
 with open("./config.json") as f:
     conf = json.loads(f.read())
@@ -14,17 +14,24 @@ readKey = conf["source"]["ambient_key_read"]
 
 url = f"https://ambidata.io/api/v2/channels/{channelId}/data?&readKey={readKey}&n=1"
 
-res = requests.get(url)
-print(res.status_code)
 
-d = res.json()[0]
+while True:
+    res = requests.get(url)
+    print(f"res.code:{res.status_code}")
+    if res.status_code == 200:
 
-data = {i:d[i] for i in d.keys() if i in dataKeys}
+        d = res.json()[0]
+        data = {i:d[i] for i in d.keys() if i in dataKeys}
 
-for i in conf["clients"]:
-    am = ambient.Ambient(i["ambient_channel"],i["ambient_key_write"])
-    res = am.send(data,timeout=3)
-    time.sleep(3)
-    print(f"ch:{i['ambient_channel']},key:{i['ambient_key_write']}")
+        for i in conf["clients"]:
+            am = ambient.Ambient(i["ambient_channel"],i["ambient_key_write"])
+            res = am.send(data,timeout=3)
+            time.sleep(1)
+            print(f"res:{res},ch:{i['ambient_channel']},key:{i['ambient_key_write']}")
 
-print(data)
+        print(data)
+
+    if conf["interval"] == 0:
+        break
+    else:
+        time.sleep(conf["interval"])
